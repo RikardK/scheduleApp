9204139f76f05b2bff9fc4103b8d48e8c30ab00d
RikardK <rikard88karlsson@gmail.com>
2013-05-01 17:36:58 +0200

    Validation of student info is now working

--- scheduleApp/Student.m
+++ scheduleApp/Student.m
@@ -21,18 +21,47 @@
 
     if (self) {
         NSString *stringValidation = @"[a-z]*";
-        NSPredicate *predicate = [NSPredicate predicateWithFormat:@"SELF MATCHES[C] %@", stringValidation];
+        NSString *numberValidation = @"[0-9]{1,7}";
+        NSString *emailValidation = @"[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}";
+        NSString *ageValidation = @"[0-9]{2}";
+        NSPredicate *emailPredicate = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", emailValidation];
+        NSPredicate *stringPredicate = [NSPredicate predicateWithFormat:@"SELF MATCHES[C] %@", stringValidation];
+        NSPredicate *numberPredicate = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", numberValidation];
+        NSPredicate *agePredicate = [NSPredicate predicateWithFormat:@"SELF MATCHES %@", ageValidation];
         
-        if ([predicate evaluateWithObject:firstName] && [predicate evaluateWithObject:lastName]) {
+        NSArray *addressArray = [address componentsSeparatedByString:@", "];
+        NSArray *firstPartOfAddress = [[addressArray objectAtIndex:0] componentsSeparatedByString:@" "];
+        NSArray *secondPartOfAddress = [[addressArray objectAtIndex:1] componentsSeparatedByString:@" "];
+        
+        
+        if ([stringPredicate evaluateWithObject:firstName] && [stringPredicate evaluateWithObject:lastName]) {
             self.firstName = firstName;
             self.lastName = lastName;
         } else {
-            [NSException raise:@"Invalid format, may only contain letters a through z" format:@"First name: %@ or Last name: %@", firstName, lastName];
+            [NSException raise:@"Invalid format, may only contain letters a through z" format:@"First name sent in: %@ or Last name sent in: %@", firstName, lastName];
         }
         
-        self.age = age;
-        self.email = email;
-        self.address = address;
+        if ([emailPredicate evaluateWithObject:email]) {
+            self.email = email;
+        } else {
+            [NSException raise:@"Invalid e-mail address, format should be: XXXX@XXX.XX / XXXX@XXXX.XXX" format:@"E-mail address sent in: %@", email];
+        }
+        
+        if ([stringPredicate evaluateWithObject:[firstPartOfAddress objectAtIndex:0]] &&
+            [numberPredicate evaluateWithObject:[firstPartOfAddress objectAtIndex:1]] &&
+            [numberPredicate evaluateWithObject:[secondPartOfAddress objectAtIndex:0]] &&
+            [stringPredicate evaluateWithObject:[secondPartOfAddress objectAtIndex:1]]) {
+            self.address = address;
+        } else {
+            [NSException raise:@"Invalid address, format should be: street name street number, zip code city/town" format:@"Address sent in: %@", address];
+        }
+        
+        if ([agePredicate evaluateWithObject:[NSString stringWithFormat:@"%lu", age]]) {
+            self.age = age;
+        } else {
+            [NSException raise:@"Invalid age!" format:@"Age sent in: %lu", age];
+        }
+        
         self.type = @"Student";
         self.courses = @[@"App development", @"C programming"];
     }
